f328c69d0ffd59d6c3224e8c7585f9c92e970e87
tomgause <71714664+tomgause@users.noreply.github.com>
2022-05-08 12:42:35 -0400

    Changed to ada2

--- smallpart.sh
+++ smallpart.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-gpart create -s GPT ada1
+gpart create -s GPT ada2
 gpart add -t freebsd-ufs -a 1M ada1
 
-newfs -U /dev/ada1p1
+newfs -U /dev/ada2p1
 mkdir /newdisk
-sudo mount /dev/ada1p1 /newdisk
+sudo mount /dev/ada2p1 /newdisk
 
-gpart show ada1
+gpart show ada2
 
 mkdir /newdisk/dir1
 
